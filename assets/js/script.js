15ff0c424243a9518cd5e295654536b92172f7a5
Robin Jonval <88200918+nibor51@users.noreply.github.com>
2023-01-07 09:35:27 +0100

    test opti code with simple question for gpt3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,88 +1,74 @@
-const display = document.querySelector('.display');
+const display = document.getElementById('display');
 
-const keypadClearButton = document.querySelector('.keypad-button-clear');
+const keypadClearButton = document.getElementById('keypad-button-clear');
+const keypadEqualsButton = document.getElementById('keypad-button-equals');
+const keypadAddButton = document.getElementById('keypad-button-add');
+const keypadSubtractButton = document.getElementById('keypad-button-subtract');
+const keypadMultiplyButton = document.getElementById('keypad-button-multiply');
+const keypadDivideButton = document.getElementById('keypad-button-divide');
+const keypadDecimalButton = document.getElementById('keypad-button-decimal');
+const keypadZeroButton = document.getElementById('keypad-button-zero');
+const keypadOneButton = document.getElementById('keypad-button-one');
+const keypadTwoButton = document.getElementById('keypad-button-two');
+const keypadThreeButton = document.getElementById('keypad-button-three');
+const keypadFourButton = document.getElementById('keypad-button-four');
+const keypadFiveButton = document.getElementById('keypad-button-five');
+const keypadSixButton = document.getElementById('keypad-button-six');
+const keypadSevenButton = document.getElementById('keypad-button-seven');
+const keypadEightButton = document.getElementById('keypad-button-eight');
+const keypadNineButton = document.getElementById('keypad-button-nine');
 
-const keypadEqualsButton = document.querySelector('.keypad-button-equals');
+const keypadButtonsArray = [  keypadZeroButton,  keypadOneButton,  keypadTwoButton,  keypadThreeButton,  keypadFourButton,  keypadFiveButton,  keypadSixButton,  keypadSevenButton,  keypadEightButton,  keypadNineButton,  keypadDecimalButton];
 
-const keypadAddButton = document.querySelector('.keypad-button-add');
-const keypadSubtractButton = document.querySelector('.keypad-button-subtract');
-const keypadMultiplyButton = document.querySelector('.keypad-button-multiply');
-const keypadDivideButton = document.querySelector('.keypad-button-divide');
+const keypadOperatorsArray = [  keypadAddButton,  keypadSubtractButton,  keypadMultiplyButton,  keypadDivideButton];
 
-const keypadDecimalButton = document.querySelector('.keypad-button-decimal');
-const keypadZeroButton = document.querySelector('.keypad-button-zero');
-const keypadOneButton = document.querySelector('.keypad-button-one');
-const keypadTwoButton = document.querySelector('.keypad-button-two');
-const keypadThreeButton = document.querySelector('.keypad-button-three');
-const keypadFourButton = document.querySelector('.keypad-button-four');
-const keypadFiveButton = document.querySelector('.keypad-button-five');
-const keypadSixButton = document.querySelector('.keypad-button-six');
-const keypadSevenButton = document.querySelector('.keypad-button-seven');
-const keypadEightButton = document.querySelector('.keypad-button-eight');
-const keypadNineButton = document.querySelector('.keypad-button-nine');
+const keypadOperators = [  '+',  '-',  'x',  '/'];
 
-const keypadButtonsArray = [
-    keypadZeroButton,
-    keypadOneButton,
-    keypadTwoButton,
-    keypadThreeButton,
-    keypadFourButton,
-    keypadFiveButton,
-    keypadSixButton,
-    keypadSevenButton,
-    keypadEightButton,
-    keypadNineButton,
-    keypadDecimalButton
-];
+function updateDisplay(button) {
+  if (display.textContent === '0') {
+    display.textContent = button.textContent;
+  } else {
+    display.textContent += button.textContent;
+  }
+}
 
-const keypadOperatorsArray = [
-    keypadAddButton,
-    keypadSubtractButton,
-    keypadMultiplyButton,
-    keypadDivideButton
-];
+function updateOperator(button) {
+  for (let j = 0; j < keypadOperators.length; j++) {
+    if (display.textContent.includes(keypadOperators[j]) === false) {
+      display.textContent += button.textContent;
+      break;
+    } else if (display.textContent.charAt(display.textContent.length - 1) === keypadOperators[j]) {
+      display.textContent = display.textContent.slice(0, -1) + button.textContent;
+      break;
+    }
+  }
+}
 
-const keypadOperators = [
-    '+',
-    '-',
-    'x',
-    '/'
-];
+function clearDisplay() {
+  display.textContent = '0';
+}
 
+function calculateResult() {
+  if (display.textContent.includes('x')) {
+    display.textContent = display.textContent.replace('x', '*');
+  }
+  let expression = display.textContent;
+  let result = eval(expression);
+  display.textContent = result;
+}
 
 for (let i = 0; i < keypadButtonsArray.length; i++) {
-    keypadButtonsArray[i].addEventListener('click', function () {
-        if (display.textContent === '0') {
-            display.textContent = this.textContent;
-        } else {
-            display.textContent += this.textContent;
-        }
-    });
+  keypadButtonsArray[i].addEventListener('click', function() {
+    updateDisplay(this);
+  });
 }
 
 for (let i = 0; i < keypadOperatorsArray.length; i++) {
-    keypadOperatorsArray[i].addEventListener('click', function () {
-        for (let j = 0; j < keypadOperators.length; j++) {
-            if (display.textContent.includes(keypadOperators[j]) === false) {
-                display.textContent += this.textContent;
-                break;
-            } else if (display.textContent.charAt(display.textContent.length - 1) === keypadOperators[j]) {
-                display.textContent = display.textContent.slice(0, -1) + this.textContent;
-                break;
-            }
-        }
-    });
+  keypadOperatorsArray[i].addEventListener('click', function() {
+    updateOperator(this);
+  });
 }
 
-keypadClearButton.addEventListener('click', function () {
-    display.textContent = '0';
-});
+keypadClearButton.addEventListener('click', clearDisplay);
+keypadEqualsButton.addEventListener('click', calculateResult);
 
-keypadEqualsButton.addEventListener('click', function () {
-    if (display.textContent.includes('x')) {
-        display.textContent = display.textContent.replace('x', '*');
-    }
-    let expression = display.textContent;
-    let result = eval(expression);
-    display.textContent = result;
-});
